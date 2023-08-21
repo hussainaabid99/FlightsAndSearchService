508f04b83f64a73abb3a58a6fd941c8c6d1241bd
Aabid <hussain.aabid2002@gmail.com>
2023-08-21 19:16:12 +0530

    Added routers to complete create api

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -1,10 +1,10 @@
-const { CityService, CityService } = require('../services/index');
+const { CityService } = require('../services/index');
 
 const cityService = new CityService();
 
 const create = async (req, res) => {
           try {
-                    const city = await CityService.createCity(req.body);
+                    const city = await cityService.createCity(req.body);
                     return res.status(201).json({
                               data: city,
                               success: true,
@@ -24,7 +24,7 @@ const create = async (req, res) => {
 
 const destroy = async (req, res) => {
           try {
-                    const response = await CityService.deleteCity(req.params.id);
+                    const response = await cityService.deleteCity(req.params.id);
                     return res.status(200).json({
                               data: response,
                               success: true,
@@ -45,7 +45,7 @@ const destroy = async (req, res) => {
 
 const get = async (req, res) => {
           try {
-                    const response = await CityService.getCity(req.params.id);
+                    const response = await cityService.getCity(req.params.id);
                     return res.status(200).json({
                               data: response,
                               success: true,
@@ -65,7 +65,7 @@ const get = async (req, res) => {
 
 const update = async (req, res) => {
           try {
-                    const response = await CityService.deleteCity(req.params.id, req.body);
+                    const response = await cityService.deleteCity(req.params.id, req.body);
                     return res.status(200).json({
                               data: response,
                               success: true,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 
-const { PORT } = require('./config/serverConfig')
+const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async () => {
 
@@ -10,6 +11,8 @@ const setupAndStartServer = async () => {
 
           app.use(bodyParser.json());
           app.use(bodyParser.urlencoded({ extended: true }));
+
+          app.use('/api', ApiRoutes);
 
           app.listen(PORT, () => {
                     console.log(`Server started at ${PORT}`);
