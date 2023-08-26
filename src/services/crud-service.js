fa59b641fb74cd1b9f13cf1314ac64d9beb8f3b9
Aabid <hussain.aabid2002@gmail.com>
2023-08-26 20:27:00 +0530

    Added common crud classes for basic crud operations

---
+++ src/services/crud-service.js
@@ -0,0 +1,55 @@
+class CrudService {
+          constructor(repository) {
+                    this.repository = repository;
+          }
+
+          async create(data) {
+                    try {
+                              const response = await this.repository.create(data);
+                              return response;
+                    } catch (error) {
+                              console.log("Something went wrong  in crud service");
+                    }
+          }
+
+          async destroy(id) {
+                    try {
+                              const response = await this.repository.destroy(id);
+                              return response;
+                    } catch (error) {
+                              console.log("Something went wrong  in crud service");
+                    }
+          }
+
+          async get(id) {
+                    try {
+                              const response = await this.repository.get(id);
+                              return response;
+                    } catch (error) {
+                              console.log("Something went wrong  in crud service");
+                    }
+          }
+
+          async getAll() {
+                    try {
+                              const response = await this.repository.getAll();
+                              return response;
+                    } catch (error) {
+                              console.log("Something went wrong  in crud service");
+                    }
+
+          }
+
+          async update(id, data) {
+                    try {
+                              const response = await this.repository.update(id, data);
+                              return response;
+                    } catch (error) {
+                              console.log("Something went wrong  in crud service");
+                    }
+          }
+
+
+}
+
+module.exports = CrudService;
